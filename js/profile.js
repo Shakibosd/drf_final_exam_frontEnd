adbee2bb53521596e38cb19c8b1b2ebd82aedc37
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-10-23 14:48:05 +0600

    ui design added

--- js/profile.js
+++ js/profile.js
@@ -36,14 +36,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
       flowerCard.innerHTML = `
         <div class="card bg-white text-dark pt-3 index_flower_card" style="border-radius:15px;">
-          <img src="${flower.image}" class="img-fluid mx-auto d-block" alt="${flower.title}" style="width:80%; height:210px; border-radius:10px;">
+          <img src="${flower.image}" class="img-fluid mx-auto d-block" alt="${
+        flower.title
+      }" style="width:80%; height:210px; border-radius:10px;">
           <div class="card-body">
             <h6 class="card-title">Title: ${flower.title}</h6>
             <p>Price: ${flower.price} ৳</p>
-            Category: <small class="btn btn-secondary btn-sm">${flower.category}</small>
+            Category: <small class="btn btn-secondary btn-sm">${
+              flower.category
+            }</small>
             <p>Stock: ${flower.stock}</p>
-            <p class="card-text">Description: ${flower.description.slice(0, 20)}...</p>
-            <a class="btn btn-primary btn-sm w-50" href="./flower_details.html?id=${flower.id}">Details</a>
+            <p class="card-text">Description: ${flower.description.slice(
+              0,
+              20
+            )}...</p>
+            <a class="btn btn-primary btn-sm w-50" href="./flower_details.html?id=${
+              flower.id
+            }">Details</a>
           </div>
         </div>
       `;
@@ -53,6 +62,45 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const flowerCountElement = document.getElementById("flower-count");
     flowerCountElement.innerText = `Total Flowers: ${flowers.length} !`;
-    flowerCountElement.style.color = "unset"
+    flowerCountElement.style.color = "unset";
   }
+});
+
+// flower tips
+document.addEventListener("DOMContentLoaded", function () {
+  const careTipsContainer = document.getElementById("care-tips");
+
+  const apiURL = "https://flower-seal-backend.vercel.app/flowers/care-tips/";
+
+  fetch(apiURL)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((tip) => {
+        const tipCard = document.createElement("div");
+        tipCard.classList.add("care-tip-card");
+
+        tipCard.innerHTML = `
+                  <h2>Plant Name : ${tip.plant_name}</h2>
+                  <p><strong>Symptoms : </strong> ${tip.symptoms}</p>
+                  <p><strong>Revival Steps : </strong> ${
+                    tip.revival_steps
+                  }</p>
+                  <p><strong>Recommended Fertilizer : </strong> ${tip.recommended_fertilizer}</p>
+                  <p><strong>Watering Caution : </strong> ${tip.watering_caution}</p>
+                  <p><strong>Sunlight Adjustment : </strong> ${tip.sunlight_adjustment}</p>
+                  <p><strong>Sunlightn Needs : </strong> ${tip.sunlight_needs}</p>
+                  <p><strong>Recommended Water Frequency : </strong> ${tip.recommended_water_frequency}</p>
+                  <p><strong>Created At : </strong> ${tip.created_at}</p>
+                  <p><strong>Updated At : </strong> ${tip.updated_at}</p>
+                  <p class="special-notes"><strong>Special Notes : </strong> ${
+                    tip.special_notes || "No special notes"
+                  }</p>
+              `;
+
+        careTipsContainer.appendChild(tipCard);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching care tips:", error);
+    });
 });
