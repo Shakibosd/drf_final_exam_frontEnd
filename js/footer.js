5bbbb5c0c799ca9e7b4e1b55f582ee94b079c0e6
shakibosd <syednazmusshakib94@gmail.com>
2025-01-27 05:49:40 +0600

    footer daynamic

--- js/footer.js
+++ js/footer.js
@@ -3,4 +3,9 @@ fetch("footer.html")
   .then((res) => res.text())
   .then((data) => {
     document.getElementById("footer").innerHTML = data;
+    //footer current years
+    const currentYear = new Date().getFullYear();
+    console.log(currentYear);
+    const footerText = document.getElementById("footer-text-year");
+    footerText.innerHTML = `&copy; ${currentYear} Develop With By <b>Nazmus Shakib</b>`;
   });
